37a7596c8e67b251dfb226fac925c8e850136995
Dennis Chung <chungdennis6@gmail.com>
2019-02-21 23:07:49 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,9 +37,9 @@ end
 
 # Define your play method below
 def play(board)
-  turns = 0
-  until turns == 9
-    turn(board)
-    turns += 1 
+i = 0
+  while i < 9
+  turn(board)
+  i += 1
   end
 end
